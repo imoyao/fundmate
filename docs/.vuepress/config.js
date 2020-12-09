dbe90675f7063fe96fc413d8091998c7d5a73e39
imoyao <immoyao@gmail.com>
2020-12-10 00:27:28 +0800

    创建docs分支，以后文档都放在这个分支下面更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,24 @@
 module.exports = {
   title: '基伴',
-  description: '您的基金理财好伙伴！'
+  description: '基金理财好伙伴！',
+  theme: 'antdocs',
+  themeConfig: {
+    sidebar: [
+      ['/', '简介'],
+      ['/user','使用'],
+      ['/dev','开发'],
+      ['/guide','部署'],
+      ['/api','接口'],
+      '/link'
+    ],
+    lastUpdated: true,
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'imoyao/fundmate',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    // docsBranch: 'doc',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true
+  }
 }
