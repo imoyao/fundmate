6df5c8cb20570db0cb2c6dae5b186ecf344c9464
imoyao <immoyao@gmail.com>
2021-01-03 23:42:01 +0800

    favor

--- frontend-ts/src/utils/calculate.ts
+++ frontend-ts/src/utils/calculate.ts
@@ -1,4 +1,4 @@
-// TODO: 为什么不能import
+// TODO: 为什么不能import see also: articleStatusFilter
 export function splitFloatHandler(splitFloat:number) {
   let splitArr: any
   splitArr = String(splitFloat).split('.')
---
+++ frontend-ts/src/views/FundMgr/index.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'index'
+})
+
+export default class extends Vue {
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
