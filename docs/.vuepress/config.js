107686e274318a494c17943688f5bebb97a9dc7f
imoyao <immoyao@gmail.com>
2021-01-05 21:23:14 +0800

    update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,19 @@ module.exports = {
     theme: 'antdocs',
     // 释出目录
     dest: 'docs/public',
-    plugins: {
-        "vuepress-plugin-auto-sidebar": {}
-    },
+    plugins: [
+        "vuepress-plugin-auto-sidebar", {},
+        ['homebadge', {
+            selector: '.hero',
+            repoLink: 'https://github.com/zpfz/vuepress-theme-antdocs',
+            badgeLink: 'https://img.shields.io/badge/link-996.icu-red.svg',
+            // badgeGroup: [
+            //     'https://img.shields.io/badge/build-passing-brightgreen?style=flat-square',
+            //     'https://img.shields.io/npm/dt/vuepress-theme-antdocs?style=flat-square&color=red',
+            //     'https://img.shields.io/github/license/zpfz/vuepress-theme-antdocs?style=flat-square&color=blue',
+            //     'https://img.shields.io/npm/v/vuepress-theme-antdocs?style=flat-square'
+            // ]
+        }]],
     themeConfig: {
         nav: [
             {text: '首页', link: '/'},
@@ -17,13 +27,13 @@ module.exports = {
             {
                 text: '更多',
                 items: [
-                  { text: '反馈', link:'/feedback/' },
-                  { text: 'FAQ', link:'/faq/' },
-                  { text: '隐私政策', link:'/privacy/' },
+                    {text: '反馈', link: '/feedback/'},
+                    {text: 'FAQ', link: '/faq/'},
+                    {text: '隐私政策', link: '/privacy/'},
                     {text: '开发计划', link: '/todo/'},
                 ]
-              }
-            ],
+            }
+        ],
         lastUpdated: true,
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
         repo: 'imoyao/fundmate',
