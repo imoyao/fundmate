18a08a1bac5667873f52c45cd191b12da462f9a4
imoyao <immoyao@gmail.com>
2021-02-04 23:54:24 +0800

    add cards

--- frontend/src/shims-vue.d.ts
+++ frontend/src/shims-vue.d.ts
@@ -1,4 +1,23 @@
+//shims-vue.d.ts是为了 typescript 做的适配定义文件，因为.vue 文件不是一个常规的文件类型，ts 是不能理解 vue 文件是干嘛的，
+// 加这一段是是告诉 ts，vue 文件是这种类型的。
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare module 'element-ui/lib/locale/lang/*' {
+  export const elementLocale: any
+}
+
+declare module '*.gif' {
+  export const gif: any
+}
+
+// TODO: remove this part after vue-count-to has its typescript file
+declare module 'vue-count-to'
+
+// TODO: remove this part after vue2-dropzone has its typescript file
+declare module 'vue2-dropzone'
+
+// TODO: remove this part after vue-image-crop-upload has its typescript file
+declare module 'vue-image-crop-upload'
