cad29ca99f5d3f8b9f6830b70825bb84be9b9e05
imoyao <immoyao@gmail.com>
2021-02-26 18:04:46 +0800

    update

--- backend/fundmate/data/efunds/app.py
+++ backend/fundmate/data/efunds/app.py
@@ -4,4 +4,24 @@
 """
 基金类型获取
 """
-url = 'https://e.efunds.com.cn/funds' # TODO
+import pandas as pd
+
+url = 'https://e.efunds.com.cn/funds'  # TODO
+
+
+class ImportHistory:
+    """导入投资历史
+    1. 获取用户历史购买的所有基金
+    2. 根据名称获取基金code
+    3. 对于自动识别失败的基金名称，需要用户手动输入code
+    4. 后台合并数据组
+    5. 将数据导入数据库
+    """
+
+    def import_csv(self, file_path='demo.scv', sep=','):
+        """
+        读csv获取数据结果
+        :return:
+        """
+        df = pd.read_csv(file_path, sep=sep)
+        df.head()
