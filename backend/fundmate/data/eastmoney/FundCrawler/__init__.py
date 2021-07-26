5abe650424e1c4ca1f915b6f32eb63506aaf4122
imoyao <emailme8@163.com>
2021-07-26 09:40:04 +0800

    代码结构优化/调整,更新代码作者信息

--- backend/fundmate/data/eastmoney/FundCrawler/__init__.py
+++ backend/fundmate/data/eastmoney/FundCrawler/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Created by Andy at 2021/6/25 16:54
+# Created by imoyao at 2021/6/25 16:54
 """
 [Jerry1014/FundCrawler: 爬取天天基金网，辅助对投资基金的选择](https://github.com/Jerry1014/FundCrawler)
 """
