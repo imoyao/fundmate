a419bd3e79e70208055b301c071edd6f5871ab6a
imoyao <emailme8@163.com>
2021-07-26 09:50:30 +0800

    df

---
+++ backend/fundmate/base_scheme.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Andy at 2021/7/23 14:06
+"""
+公用scheme提出来
+"""
+from apiflask import Schema
+from apiflask.fields import Integer
+from apiflask.validators import Range
+
+
+class QuerySchema(Schema):
+    page = Integer(missing=1)
+    per_page = Integer(missing=20, validate=Range(max=30))
+
+
+class EmptySchema(Schema):
+    pass
