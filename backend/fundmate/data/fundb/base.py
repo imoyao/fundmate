fd1711cd9576ca37a6609a7f07d2159a62ba5d21
imoyao <emailme8@163.com>
2021-07-30 18:18:00 +0800

    韭圈儿数据抓取；TODO:对抓取结果进行重构

---
+++ backend/fundmate/data/fundb/base.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Andy at 2021/7/30 17:43
+from xalpha.cons import rpost_json
+
+from backend.fundmate.data import utils as dt_utils
+
+_HEADER_STR = '''authority: api.jiucaishuo.com
+method: POST
+path: /v2/kjtl/getbasedata
+scheme: https
+accept: application/json, text/plain, */*
+accept-encoding: gzip, deflate, br
+accept-language: zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7
+content-length: 661
+content-type: application/json;charset=UTF-8
+dnt: 1
+origin: https://funddb.cn
+sec-ch-ua: "Chromium";v="92", " Not A;Brand";v="99", "Microsoft Edge";v="92"
+sec-ch-ua-mobile: ?0
+sec-fetch-dest: empty
+sec-fetch-mode: cors
+sec-fetch-site: cross-site
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55
+'''
+
+
+class FundDB:
+
+    def kjtl(self, is_full=False):
+        url = 'https://api.jiucaishuo.com/v2/kjtl/getbasedata'
+        data = '''{"type":"pc","data_source":"xichou","version":"1.6.0",
+        "authtoken":"ddQYkiZQ087Z5Kr+ER5CmQMFCpjuC/qW","act_time":1627638543548,"tirgkjfs":"08","abiokytke":"0b",
+        "u54rg5d":"b3","kf54ge7":"e","tiklsktr4":"8","lksytkjh":"c14c","sbnoywr":"53","bgd7h8tyu54":"54",
+        "y654b5fs3tr":"5","bioduytlw":"a","bd4uy742":"f","h67456y":"1c1","bvytikwqjk":"54","ngd4uy551":"c1",
+        "bgiuytkw":"9f","nd354uy4752":"d","ghtoiutkmlg":"513","bd24y6421f":"31","tbvdiuytk":"1","ibvytiqjek":"45",
+        "jnhf8u5231":"9f","fjlkatj":"b35","hy5641d321t":"1f","iogojti":"1","ngd4yut78":"13","nkjhrew":"f",
+        "yt447e13f":"4","n3bf4uj7y7":"1","nbf4uj7y432":"0b","yi854tew":"2d","h13ey474":"2de","quikgdky":"7d"} '''
+        hd = dt_utils.parse_headers(_HEADER_STR)
+        resp = rpost_json(url, headers=hd, data=data)
+
+        info = None
+        if resp and resp.get('code') == 0:
+            data = resp.get('data')
+
+            current_date = data.get('current_time')
+            temper = data.get('num')
+            desc = data.get('status_str')
+
+            info = {
+                'update_date': current_date,
+                'temper': temper,
+                'desc': desc,
+            }
+            if is_full:
+                detail_list = data.get('list')
+                for item in detail_list:
+                    print(item)
+                amend_info = {}
+
+        return info
+
+
+jq_app = FundDB()
+if __name__ == '__main__':
+    print(jq_app.kjtl())
