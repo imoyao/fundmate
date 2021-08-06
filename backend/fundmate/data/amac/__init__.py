e659c1b883457d86e39459552abd9c624aa4beb7
imoyao <emailme8@163.com>
2021-08-06 18:27:57 +0800

    update style’

--- backend/fundmate/data/amac/__init__.py
+++ backend/fundmate/data/amac/__init__.py
@@ -6,4 +6,4 @@ init 中不要放太多东西
 [python - Can I put a class definition into __init__.py? - Stack Overflow](https://stackoverflow.com/questions/47740935/can-i-put-a-class-definition-into-init-py)
 [What kind of code should I write/put in ‘__init__.py’ in Python applications? - Quora](https://www.quora.com/What-kind-of-code-should-I-write-put-in-init__-py-in-Python-applications)
 """
-from .base import *
+from .base import *  # noqa:F401
