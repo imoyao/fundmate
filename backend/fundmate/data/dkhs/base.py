c087d25b6a04996abb108605131e816909f08a96
imoyao <emailme8@163.com>
2021-08-18 18:00:30 +0800

    基金决策宝数据抓取，主要是费率信息的更新

--- backend/fundmate/data/dkhs/base.py
+++ backend/fundmate/data/dkhs/base.py
@@ -9,9 +9,9 @@ from xalpha.cons import rget_json
 
 from backend.fundmate import settings
 from backend.fundmate.data.utils import data_parser
-from backend.fundmate.excepts import UnexpectedArgsError
+from backend.fundmate.excepts import EmptyError, UnexpectedArgsError
 from backend.fundmate.exts.flask_loguru import logger
-from backend.fundmate.fund.models import Fund
+from backend.fundmate.fund.models import FeeRatio, Fund, InRule, OutRule
 
 abs_current_path = Path.cwd().resolve()
 FUND_SYMBOLS_SAVE_FP = f'{str(abs_current_path)}/fund_symbols.json'
@@ -28,9 +28,10 @@ class DKHS:
         _resp = rget_json(_url)
         return _resp
 
-    def fund_symbols(self):
+    def fatch_fund_info(self):
         """
         基金编码对应特殊编号写入数据库（从网站抓取之后直接写入）
+        此操作在写入数据库的同时还会写入json文件
         ---
 
         可以用到的字段：
@@ -38,6 +39,7 @@ class DKHS:
         charge_mode     收费模式（前端/后端）
         investment_risk     风险等级
         fund_name   全名
+        abbr_name   通用名称
 
         ---
 
@@ -209,11 +211,17 @@ class DKHS:
             json.dump(raw_data, f, ensure_ascii=False)
         return symbols_lists
 
-    def update_fund_info(self, code, symbol: str, charge_mode: int, investment_risk: Union[int, str], abbr_name: str):
+    @staticmethod
+    def update_fund_info(code: str, symbol: str, charge_mode: int, investment_risk: Union[int, str], abbr_name: str):
         """
         更新某基金信息（包括前缀、风险等级，收费模式<前端（1）/后端（0）>）
+        :param code: 基金编码
+        :param symbol: 更新信息之前缀，来自基金决策宝
+        :param charge_mode:收费模式，前端（1）或者后端（0）
+        :param investment_risk:风险等级
+        :param abbr_name:基金名称（非全称）
+        :return:
         """
-
         if charge_mode == 2:  # 源数据中用1/2表示前端和后端，我们强制改为0/1,其中1为前端
             charge_mode = 0
         investment_risk = int(investment_risk) if isinstance(investment_risk, str) else investment_risk
@@ -224,7 +232,13 @@ class DKHS:
             is_usable_symbol = symbol_prefix in settings.SYMBOL_TYPE.keys()
             is_usable_charge_mode = charge_mode in [0, 1]
             if all([is_usable_risk, is_usable_symbol, is_usable_charge_mode]):
-                fund_inst.update(symbol_prefix=symbol_prefix, risk_level=investment_risk, is_fe_charge_mode=charge_mode)
+                if symbol_prefix == 'UN':
+                    # 如果是未知，则默认值没有必要更新
+                    fund_inst.update(risk_level=investment_risk, is_fe_charge_mode=charge_mode)
+                else:
+                    fund_inst.update(symbol_prefix=symbol_prefix,
+                                     risk_level=investment_risk,
+                                     is_fe_charge_mode=charge_mode)
                 return fund_inst
             else:
                 raise UnexpectedArgsError(f'Please check your arguments:investment_risk:{investment_risk},'
@@ -232,9 +246,43 @@ class DKHS:
         else:
             logger.error(f'<Fund({code!r}, {abbr_name!r})> info get failed from DB,please check it.')
 
+    @staticmethod
+    def search_symbol(fund_code: str) -> Union[str, None]:
+        """
+        通过搜索接口查询symbol
+        :param fund_code: 基金编码
+        :return:
+        """
+        # 特殊处理  FIXME：退市基金此处会获取失败，返回信息类似：FP**Z；还有一部分查询的返回结果是信托产品，如:202010
+        special_list = ['202010']
+        if fund_code in special_list:
+            raise ValueError(f'The code:{fund_code} will return incorrect info,we will skip it.')
+        _url = f'https://www.dkhs.com/api/v1/se' \
+               f'arch/symbols/?symbol_type=3&page_size=1&q={fund_code}'
+        _resp = rget_json(_url)
+        if _resp:
+            results = _resp.get('results')
+            if results:
+                fund_obj = results[0]
+                symbol = fund_obj.get('symbol')
+                code = fund_obj.get('code')
+                if code == fund_code:
+                    symbol_prefix = symbol.replace(code, '')
+                    if symbol_prefix in settings.SYMBOL_TYPE.keys():
+                        return symbol_prefix
+                else:
+                    logger.info(f'Get unexpected code:{code}.')
+                    raise UnexpectedArgsError(f'Please check your fund code {fund_code},it returns code {code}.')
+
+                logger.error(f'Get unexpected symbol:{symbol}.')
+            else:
+                # FIXME: 此处获取symbol失败，则交易费率信息需要手动更新或者使用别的方式进行爬取
+                logger.error(f'Get symbol error of fund code:{fund_code}.')
+                raise UnexpectedArgsError(f'Get symbol error of fund code:{fund_code}.')
+
     def parse_json_to_db(self):
         """
-        从json文件中读取文件并更新信息到数据库
+        从json文件中读取文件并更新信息到数据库，这个接口应该作为search_symbol的补充
         （目前该接口只能获取到部分基金的信息）
         """
         fund_data_list = data_parser.get_data_from_json(FUND_SYMBOLS_SAVE_FP)
@@ -249,12 +297,101 @@ class DKHS:
                 logger.success(f'Update {fund_inst} successfully.')
         return 0
 
-    def fee_raito(self, fund_code: str):
+    def transfer_rule(self, fund_id: str, fee_type: int, fee_amount: float, rule_item: dict, rule_info: dict):
+        is_in = False
+        if fee_type in [1, 2]:
+            is_in = True
+            rule_class = InRule
+        else:
+            rule_class = OutRule
+        _rule_inst = rule_class.insert_or_update(rule_info, **rule_info)
+        rule_id = _rule_inst.id
+        fare_ratio = rule_item.get('fare_ratio')
+        if fare_ratio:
+            rate = float(fare_ratio)
+        else:
+            rate = None
+        rate_info = {
+            'fund_id': fund_id,
+            'rule_id': rule_id,
+            'fee_type': fee_type,
+            'rate': rate,
+            'fee_amount': fee_amount,
+        }
+        if is_in:
+            rate_info.update({'in_rule_id': rule_id})
+        else:
+            rate_info.update({'out_rule_id': rule_id})
+        return rate_info
+
+    def fee_ratio(self, fund_code: str):
         """
-        [兴全合润混合(SZ163406)_基金净值_费率_行情走势](https://www.dkhs.com/s/SZ163406/) “交易须知” 子页面
+        根据基金决策宝网站信息更新费率
+        数据来源：[兴全合润混合(SZ163406)_基金净值_费率_行情走势](https://www.dkhs.com/s/SZ163406/) “交易须知” 子页面
+        :param fund_code: 基金编码
+        :return:
         """
-        _url = f'https://www.dkhs.com/api/v1/symbols/FP{fund_code}/fare_ratio/'
+        symbol_prefix = 'UN'
+        _fund_inst = Fund.filter_by_code(fund_code)
+        if _fund_inst:
+            symbol_prefix = _fund_inst.symbol_prefix
+
+        if symbol_prefix == 'UN':
+            raise UnexpectedArgsError(f'The symbol of fund {fund_code} is unknown,Please check or update it.')
+
+        _url = f'https://www.dkhs.com/api/v1/symbols/{symbol_prefix}{fund_code}/fare_ratio/'
         _resp = rget_json(_url)
+        fund_id = _fund_inst.id
+        if _resp:
+            if isinstance(_resp, dict):
+                errors = _resp.get('errors')
+                if errors:
+                    err_msg = errors.get('symbol_error')
+                    raise UnexpectedArgsError(err_msg)
+
+            for item in _resp:
+                direction = item.get('direction')
+                fee_amount = None
+                # 申购和买入
+                if direction in [0, 6]:
+                    min_balance = item.get('min_balance')
+                    max_balance = item.get('max_balance')
+                    if direction == 0:
+                        dir_type = 'subscribe'
+                    else:
+                        dir_type = 'purchase'
+                    fee_type = settings.FEE_TYPE.get(dir_type)
+                    # 最大值时按照固定收费，同时修改上线为正无穷
+
+                    if max_balance == '0.00':
+                        min_fare = item.get('min_fare')
+                        max_fare = item.get('max_fare')
+                        if min_fare == max_fare:
+                            fee_amount = float(min_fare)
+                            max_balance = None
+                    # 更新规则表
+                    rule_info = {
+                        'start_quota': min_balance,
+                        'end_quota': max_balance,
+                    }
+
+                elif direction == 1:
+                    fee_type = settings.FEE_TYPE.get('redeem')
+                    start_day = item.get('min_hold')
+                    end_day = item.get('max_hold')
+
+                    rule_info = {
+                        'start_day': start_day,
+                        'end_day': end_day,
+                    }
+
+                rate_info = self.transfer_rule(fund_id, fee_type, fee_amount, item, rule_info)
+                if rate_info:
+                    rule_id = rate_info.pop('rule_id')
+                    query_args = {'fund_id': fund_id, 'rule_id': rule_id, 'fee_type': fee_type}
+                    FeeRatio.insert_or_update(query_args, do_log_flag=True, **rate_info)
+        else:
+            raise EmptyError(f'The href: {_url} from remote get empty response.')
         return _resp
 
 
