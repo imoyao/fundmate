a4d6416b9e0871eb847a935122245682234dbf9b
imoyao <emailme8@163.com>
2021-12-20 14:00:15 +0800

    手动增加一部分组合

---
+++ backend/fundmate/data/fund_portfolios.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@create: 2021/12/20 10:55
+@file: fund_portfolios.py
+@author: imoyao
+@email: immoyao@gmail.com
+@desc: 基金组合信息入库
+
+一些备忘链接：[深入分析15个基金组合之后，我有这些发现-雪球](https://xueqiu.com/4778574435/199049616?page=15)
+"""
