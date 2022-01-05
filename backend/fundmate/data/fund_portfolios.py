62a76d568c8dac232287d9a1c649df282ea42d58
imoyao <emailme8@163.com>
2022-01-05 18:15:37 +0800

    代码未验证测试！基金组合添加好买基金平台；组合初始化支持通过指定flag来更新部分平台的基金组合

--- backend/fundmate/data/fund_portfolios.py
+++ backend/fundmate/data/fund_portfolios.py
@@ -8,6 +8,10 @@
 @desc:
 1. InitPortfolio类用于基金组合信息入库；
 2. UpdatePortfolio 类用于已有组合调仓信息更新
+@ update:
+- 2022-01-06 11:16:55：
+1. 基金组合添加好买基金平台；
+2. 组合初始化支持通过指定flag来更新部分平台的基金组合
 
 组合分析：[深入分析15个基金组合之后，我有这些发现-雪球](https://xueqiu.com/4778574435/199049616?page=15)
 """
@@ -17,10 +21,11 @@ from typing import Dict, List, Optional
 from flask import current_app
 
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import and_, create_engine
 
 from backend.fundmate.app import db
 from backend.fundmate.data.danjuan.combination import Strategy as DJStrategy
+from backend.fundmate.data.howbuy.combination import Strategy as HBStrategy
 from backend.fundmate.data.qieman.combination import Strategy as QMStrategy
 from backend.fundmate.database import get_table_name
 from backend.fundmate.excepts import FundQueryError, NotSupportPlatError
@@ -32,6 +37,7 @@ from backend.fundmate.fund.models import (
     FundPortfolioMgr,
 )
 from backend.fundmate.libs.pysnowflake import snowflake
+from backend.fundmate.settings import PLAT_TYPE
 
 config = current_app.config
 SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
@@ -56,12 +62,14 @@ class BasePortfolio:
     def __init__(self):
         self.dj_po = DJStrategy()
         self.qm_po = QMStrategy()
+        self.hb_po = HBStrategy()
         self.engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
     def get_strategy(self, plat_str: str):
         strategies = {
             'dj': self.dj_po,
             'qm': self.qm_po,
+            'hb': self.hb_po,
         }
         strategy_obj = strategies.get(plat_str)
         return strategy_obj
@@ -100,8 +108,13 @@ class InitPortfolio(BasePortfolio):
     初始化组合时调用该接口
     """
 
-    def __init__(self):
+    def __init__(self, is_init_qieman: bool = True, is_init_danjuan: bool = True, is_init_howbuy: bool = True):
         super().__init__()
+        self.init_config = {
+            'qm': is_init_qieman,
+            'dj': is_init_danjuan,
+            'hb': is_init_howbuy,
+        }
 
     @staticmethod
     def upsert_mgr(plat_flag: str, mgr_info: Optional[Dict] = None) -> str:
@@ -111,8 +124,13 @@ class InitPortfolio(BasePortfolio):
         :param mgr_info: 
         :return: 
         """
-        mgr_code = mgr_info.get('plat_code')
-        mgr_instance = FundPortfolioMgr.query.filter_by(plat_code=mgr_code, platform=plat_flag).one_or_none()
+        if plat_flag in ['qm', 'dj']:
+            mgr_code = mgr_info.get('plat_code')
+            mgr_instance = FundPortfolioMgr.query.filter_by(plat_code=mgr_code, platform=plat_flag).one_or_none()
+        elif plat_flag == 'hb':
+            mgr_name = mgr_info.get('name')
+            mgr_instance = FundPortfolioMgr.query.filter_by(name=mgr_name, platform=plat_flag).one_or_none()
+
         if mgr_instance is None:
             mgr_code = FundPortfolioMgr.gen_mgr_code()
             mgr_info['code'] = mgr_code
@@ -145,7 +163,7 @@ class InitPortfolio(BasePortfolio):
         trade_info = None
         if plat_flag == 'dj':
             trade_info = po_inst.pagination_trade_info(plt_code)
-        elif plat_flag == 'qm':
+        elif plat_flag in ['qm', 'hb']:  # 且慢和好买返回的信息默认最近调仓在最前面
             trade_info = po_inst.pagination_trade_info(plt_code, is_desc=False)
         if trade_info:
             portfolio_code = po_obj.portfolio_code
@@ -159,15 +177,21 @@ class InitPortfolio(BasePortfolio):
         初始化组合
         :return:
         """
-        # support_platforms = PLAT_TYPE.keys()
-        support_platforms = ['qm', 'dj']
+        # 配置为True且在支持列表中
+        support_platforms = [plt for plt in self.init_config if self.init_config[plt] is True and plt in PLAT_TYPE]
         for plat_flag in support_platforms:
             po_obj = self.get_strategy(plat_flag)
             portfolios = po_obj.list_all()
-            for po_code in portfolios:
-                fpo = self.create_portfolio(po_code, plat_flag)
-                if fpo:
-                    logger.success(f'基金组合 {fpo} 信息保存完成!')
+            if plat_flag != 'hb':
+                for po_code in portfolios:
+                    fpo = self.create_portfolio(po_code, plat_flag)
+                    if fpo:
+                        logger.success(f'基金组合 {fpo} 信息保存完成!')
+            else:
+                for po_code in portfolios:
+                    fpo = self.create_portfolio(po_code, plat_flag)
+                    if fpo:
+                        logger.success(f'基金组合 {fpo} 信息保存完成!')
 
 
 class UpdatePortfolio(BasePortfolio):
@@ -187,21 +211,8 @@ class UpdatePortfolio(BasePortfolio):
         """
         if not plt_code.startswith('CSI'):
             raise FundQueryError(f'请检查输入的平台组合编号 {plt_code} 是否正确？')
-        total = self.dj_po.total_times(plt_code)
-        record_count = FundPortfolioAdjustHistory.adjust_count(portfolio_code)
-        if total is not None:
-            if record_count < total:
-                adjust_size = total - record_count
-                # 蛋卷基金的直接获取就行，total_times接口不会返回调仓信息
-                trade_info = self.dj_po.pagination_trade_info(plt_code, size=adjust_size)
-                if trade_info:
-                    self.persist_trade_info(portfolio_code, trade_info)
-                else:
-                    logger.error(f'获取组合 {portfolio_code} 调仓信息出错，请检查确认……')
-            else:
-                logger.info(f'组合 {portfolio_code} 期间未发生调仓……')
-        else:
-            logger.error('获取组合信息出错，请检查网络连接……')
+        dj_obj = self.dj_po
+        self.update_no_total(dj_obj, plt_code, portfolio_code)
 
     def update_qieman(self, plt_code: str, portfolio_code: str):
         """
@@ -238,6 +249,33 @@ class UpdatePortfolio(BasePortfolio):
         else:
             logger.error('获取组合信息出错，请检查网络连接……')
 
+    def update_no_total(self, query_instance, plt_code: str, portfolio_code: str):
+        total = query_instance.total_times(plt_code)
+        record_count = FundPortfolioAdjustHistory.adjust_count(portfolio_code)
+        if total is not None:
+            if record_count < total:
+                adjust_size = total - record_count
+                # 直接通过获取相应数量的调仓信息，total_times接口不会返回调仓信息
+                trade_info = query_instance.pagination_trade_info(plt_code, size=adjust_size)
+                if trade_info:
+                    self.persist_trade_info(portfolio_code, trade_info)
+                else:
+                    logger.error(f'获取组合 {portfolio_code} 调仓信息出错，请检查确认……')
+            else:
+                logger.info(f'组合 {portfolio_code} 期间未发生调仓……')
+        else:
+            logger.error('获取组合信息出错，请检查网络连接……')
+
+    def update_howbuy(self, plt_code: str, portfolio_code: str):
+        """
+        周期性更新好买基金平台组合
+
+        该操作和蛋卷一致，只是调用的实例不同
+        :return:
+        """
+        hb_obj = self.hb_po
+        self.update_no_total(hb_obj, plt_code, portfolio_code)
+
     def update_portfolio(self):
         """
         更新数据库中已经记录的组合
@@ -246,7 +284,12 @@ class UpdatePortfolio(BasePortfolio):
         3. 调用persist_trade_info保存调仓记录
         :return:
         """
-        fpos = db.session.query(FundPortfolio).filter(FundPortfolio.platform != 0)
+        # 未定义和平台自有不需要更新
+        fpos = db.session.query(FundPortfolio).filter(
+            and_(
+                FundPortfolio.platform != 'undefined',
+                FundPortfolio.platform != 'own',
+            ))
         # 遍历获取组合是否调仓，如果调仓，则将其信息存入数据库
         for po_item in fpos:
             plt_code = po_item.code
@@ -258,30 +301,43 @@ class UpdatePortfolio(BasePortfolio):
             if po_last_adjust_date != last_trade_date_fmt:
                 # 调仓详情
                 po_details = po_obj.detail(plt_code)
-                risk_type = po_details.get('risk_type')
                 annualized_rate_of_return = po_details.get('annualized_rate_of_return')
                 invest_rate_of_return = po_details.get('invest_rate_of_return')
-                update_detail = {
-                    'risk_type': risk_type,
-                    'annualized_rate_of_return': annualized_rate_of_return,
-                    'invest_rate_of_return': invest_rate_of_return,
-                    'update_time': datetime.datetime.utcnow(),
-                    'last_adjust_date': last_trade_date_fmt,
-                }
+                if po_platform in ['qm', 'dj']:
+                    risk_type = po_details.get('risk_type')
+                    update_detail = {
+                        'risk_type': risk_type,
+                        'annualized_rate_of_return': annualized_rate_of_return,
+                        'invest_rate_of_return': invest_rate_of_return,
+                        'update_time': datetime.datetime.utcnow(),
+                        'last_adjust_date': last_trade_date_fmt,
+                    }
+                elif po_platform == 'hb':
+                    # 好买基金的风险信息为手动填写，不需要更新
+                    update_detail = {
+                        'annualized_rate_of_return': annualized_rate_of_return,
+                        'invest_rate_of_return': invest_rate_of_return,
+                        'update_time': datetime.datetime.utcnow(),
+                        'last_adjust_date': last_trade_date_fmt,
+                    }
                 # 更新组合基本信息
                 fpo = FundPortfolio.query.filter_by(portfolio_code=portfolio_code).one_or_none()
                 if fpo:
                     fpo.update(**update_detail)
                 if po_platform == 'qm':
                     self.update_qieman(plt_code, portfolio_code)
-
                 elif po_platform == 'dj':
                     self.update_danjuan(plt_code, portfolio_code)
+                elif po_platform == 'hb':
+                    self.update_howbuy(plt_code, portfolio_code)
 
 
 if __name__ == '__main__':
-    # 初始化调用
-    po = InitPortfolio()
+    # 初始化，需要保证在Flask上下文中调用
+    is_init_qieman = False
+    is_init_danjuan = False
+    is_init_howbuy = True
+    po = InitPortfolio(is_init_qieman, is_init_danjuan, is_init_howbuy)
     po.init_portfolio()
     # 日常更新组合调仓信息等
     update_po = UpdatePortfolio()
--- backend/fundmate/fund/models.py
+++ backend/fundmate/fund/models.py
@@ -560,7 +560,7 @@ class FundPortfolioAdjustHistory(PkModel):
     update_date = Column(db.DateTime, comment='调仓时间')
     adjust_id = Column(db.BigInteger, comment='调仓历史编码')  # 使用雪花算法
     plat_trade_id = Column(db.String(120), comment='平台调仓编码（只做记录区分用，不参与系统计算）')
-    desc = Column(db.String(300), comment='调仓说明')
+    desc = Column(db.String(1500), comment='调仓说明')
 
     def __repr__(self):
         return f'组合( {self.portfolio_code!r} ) 调仓时间： {self.update_date!r}，记录编号：{self.adjust_id!r}>'
