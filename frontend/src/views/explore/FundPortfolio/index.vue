c1d4c18c132d0a8daf69901e441385c823dd1269
imoyao <emailme8@163.com>
2022-01-10 18:07:23 +0800

    组合列表显示

---
+++ frontend/src/views/explore/FundPortfolio/index.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="app-container">
+    <el-row class="panel-group">
+        <el-select v-model="value1" multiple placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+
+        <el-select v-model="value1" multiple placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+    </el-row>
+
+    <el-row class="panel-group" :gutter="45">
+      <el-col :span="8">
+        <el-card class="box-card" shadow="hover">
+          <div slot="header" class="clearfix">
+            <span>成长五剑</span>
+            <el-tag size="mini" style="margin-left: 6px;">一石二鸟</el-tag>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div v-for="o in 4" :key="o" class="text item">
+            <span>组合描述</span>
+            <span>组合年化收益</span>
+            <span>组合年化收益</span>
+            {{ '列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card class="box-card" shadow="hover">
+          <div slot="header" class="clearfix">
+            <span>成长五剑</span>
+            <el-tag size="mini" style="margin-left: 6px;">盈米</el-tag>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div v-for="o in 4" :key="o" class="text item">
+            <span>组合描述</span>
+            <span>组合年化收益</span>
+            <span>组合年化收益</span>
+            {{ '列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card class="box-card" shadow="hover">
+          <div slot="header" class="clearfix">
+            <span>成长五剑</span>
+            <el-tag size="mini" style="margin-left: 6px;">一石二鸟</el-tag>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div v-for="o in 4" :key="o" class="text item">
+            <span>组合描述</span>
+            <span>组合年化收益</span>
+            <span>组合年化收益</span>
+            {{ '列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import DraggableCard from '@/components/DragableCard/index.vue'
+// import Record from '../Record/index.vue'
+
+@Component({
+  name: 'FundPortfolio',
+  components: {
+    DraggableCard
+  }
+})
+export default class extends Vue {
+  private options = [{
+    value: '选项1',
+    label: '黄金糕'
+  }, {
+    value: '选项2',
+    label: '双皮奶'
+  }, {
+    value: '选项3',
+    label: '蚵仔煎'
+  }, {
+    value: '选项4',
+    label: '龙须面'
+  }, {
+    value: '选项5',
+    label: '北京烤鸭'
+  }]
+
+  private value1 = '1'
+}
+</script>
+<style>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+
+.box-card {
+  width: 480px;
+}
+</style>
