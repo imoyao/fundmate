7916216c20fc11a8adb442a22839b49b6c5cef90
imoyao <emailme8@163.com>
2022-01-28 18:25:55 +0800

    支付宝个人账单处理

--- backend/fundmate/data/alipay/trans.py
+++ backend/fundmate/data/alipay/trans.py
@@ -19,8 +19,9 @@
 6. 数据集去除 whitespace
 7. 解析交易行为：获取交易性质、交易源产品、交易目标产品
 """
+import re
 from pathlib import Path
-from typing import Union
+from typing import Dict, Optional, Union
 
 import pandas as pd
 
@@ -34,38 +35,14 @@ current_path = Path.cwd()
 '''
 # PC: True MOBILE: False
 IS_FROM_PC = False
-# 手机端导出文件配置此处
-MOBILE_FILE_NAME = 'alipay_record_20220119_173409.csv'
+
+# ============手机端导出文件配置此处==============
+# MOBILE_FILE_NAME = 'alipay_record_20220119_173409.csv'
+# MOBILE_FILE_NAME = 'alipay_record_20220128_161121.csv'
+MOBILE_FILE_NAME = 'alipay_record_20220128_161241.csv'
 BASE_MOBILE_FILE_EXPORT_NAME = '手机端支付宝交易单导出.csv'
-# PC端导出文件配置此处
-PC_FILE_NAME = ''
-BASE_PC_FILE_EXPORT_NAME = 'PC端支付宝交易单导出.csv'
-
-# r'C:\Users\Andy\Desktop\alipay_record_20220119_173409\alipay_record_20220119_173409.csv'
-PC_ALIPAY_RECORDS_FP = Path(current_path).joinpath(PC_FILE_NAME)
 MOBILE_ALIPAY_RECORDS_FP = Path(current_path).joinpath(MOBILE_FILE_NAME)
-PC_ALIPAY_EXPORT_FP = Path(current_path).joinpath(PC_FILE_NAME)
-MOBILE_ALIPAY_EXPORT_FP = Path(current_path).joinpath(MOBILE_FILE_NAME)
-encoding = 'gb18030'
-CASH_NAME = '余额宝'
-TRANSFER_SYMBOL = '[转换至]'
-ANT_FORTUNE_SALE_TO_YEB = '卖出至余额宝'
-ANT_FORTUNE_PURCHASE_STR = '买入'
-ANT_FORTUNE_BONUS_TO_YEB = '现金分红至余额宝'
-ANT_FORTUNE_COMB_TO_YEB = '转出至余额宝'
-YUEBAO_LISTS = []
-# WARNING：记录流水号涉及部分个人敏感数据上传，请确保使用时是自主配置该项
-IS_RECORD_TRANSACTIONAL_NUMBER = False
-
-
-def get_raw_df(raw_fp: Union[str, Path]) -> PdDataFrame:
-    if raw_fp.exists() and raw_fp.is_file():
-        mb_df = pd.read_csv(raw_fp, header=1, encoding=encoding)
-        return mb_df
-    else:
-        raise FileNotFoundError('请正确配置导入的源文件路径！')
-
-
+MOBILE_ALIPAY_EXPORT_FP = Path(current_path).joinpath(BASE_MOBILE_FILE_EXPORT_NAME)
 # ['收/支',
 #  '交易对方',
 #  '对方账号',
@@ -82,6 +59,71 @@ mb_rename_list = [
     'op_type', 'trans_obj', 'trans_account', 'comment', 'pay_method', 'amount', 'status', 'trans_type', 'trans_code',
     'bus_code', 'trans_datetime'
 ]
+MOBILE_DROP_COLUMNS = ['trans_obj', 'trans_account', 'status', 'trans_type', 'bus_code']
+# =========PC端导出文件配置此处=========
+PC_FILE_NAME = ''
+BASE_PC_FILE_EXPORT_NAME = 'PC端支付宝交易单导出.csv'
+
+# r'C:\Users\Andy\Desktop\alipay_record_20220119_173409\alipay_record_20220119_173409.csv'
+PC_ALIPAY_RECORDS_FP = Path(current_path).joinpath(PC_FILE_NAME)
+PC_ALIPAY_EXPORT_FP = Path(current_path).joinpath(BASE_PC_FILE_EXPORT_NAME)
+
+encoding = 'gb18030'
+YEB_NAME = '余额宝'  # 余额宝，背后为货币基金
+YLB_NAME = '余利宝'  # 背后为货币基金
+'''
+每一次`ANT_FORTUNE_TRANSFER_YEB_STR` 都需要两种产品替换该字段
+'''
+YEB_NAME_OLD = '旧余额宝货币基金产品名'  # 需要导出文件后手动修改
+YEB_NAME_NEW = '新余额宝货币基金产品名'  # 需要导出文件后手动修改
+REAL_CASH = '现金'  # 即余额（可能是羊毛红包，也可能是好友转账转入）
+TRANSFER_SYMBOL = '[转换至]'
+ANT_FORTUNE_SALE_TO_YEB = '卖出至余额宝'
+ANT_FORTUNE_PURCHASE_STR = '买入'
+ANT_FORTUNE_HB_REWARD_PURCHASE_STR = '红包奖励发放'
+ANT_FORTUNE_BONUS_TO_YEB = '现金分红至余额宝'
+# 网商银行卡计息
+ANT_FORTUNE_WS_INTEREST_TO_YEB = '账户结息'
+ANT_FORTUNE_SALE_PROD_TO_YEB = '理财赎回'
+ANT_FORTUNE_PURCHASE_YEB_TO_PROD = '理财买入'
+ANT_FORTUNE_COMB_TO_YEB = '转出至余额宝'
+ANT_FORTUNE_COMB_YEB_TO_YE = '转出到余额'
+ANT_FORTUNE_AUTO_TO_YEB = '自动转入'
+ANT_FORTUNE_RE_BUY_TO_YEB = '收益发放'
+ANT_FORTUNE_PURCHASE_FUND_STR = '基金申购'
+
+ANT_FORTUNE_CHALLENGE_RATE_TO_YEB = '收益挑战'
+ANT_FORTUNE_CHALLENGE_PURCHASE_STR = '挑战包买入'
+ANT_FORTUNE_CHALLENGE_SALE_STR = '挑战包卖出'
+
+ANT_FORTUNE_REGULAR_INVEST_STR = '定期理财'
+ANT_FORTUNE_REGULAR_INVEST_SALE_STR = '定期理财赎回'
+
+ANT_FORTUNE_OUT_TO_BANK_CARD = '转出到银行卡'
+ANT_FORTUNE_BANK_CARD_IN_STR = '银行卡转入'
+ANT_FORTUNE_YLB_TO_YEB_YE = '余利宝转出到支付宝'
+ANT_FORTUNE_YEB_YE_TO_YLB = '支付宝转入到余利宝'
+ANT_FORTUNE_RECEIVED_TO_YEB = '转账收款到余额宝'
+ANT_FORTUNE_BANK_CARD_TO_YEB = '单次转入'
+ANT_FORTUNE_BANK_CARD_BIG_TO_YEB = '大额转入'
+ANT_FORTUNE_BANK_CARD_SALARY_TO_YEB = '工资理财'
+ANT_FORTUNE_MYXY_MANUAL_TO_YEB = '蚂蚁星愿主动攒入'
+ANT_FORTUNE_MYXY_AUTO_TO_YEB = '蚂蚁星愿自动攒入'
+ANT_FORTUNE_QIAN_MGR_TO_YEB = '钱管家转入'
+ANT_FORTUNE_TRANSFER_YEB_STR = '更换货基转入'
+ANT_FORTUNE_BBZ_PART1_STR = '笔笔攒'
+ANT_FORTUNE_BBZ_PART2_STR = '单笔攒入'
+YUEBAO_LISTS = []
+# WARNING：记录流水号涉及部分个人敏感数据上传，请确保使用时是自主配置该项
+IS_RECORD_TRANSACTIONAL_NUMBER = False
+
+
+def get_raw_df(raw_fp: Union[str, Path]) -> PdDataFrame:
+    if raw_fp.exists() and raw_fp.is_file():
+        mb_df = pd.read_csv(raw_fp, header=1, encoding=encoding)
+        return mb_df
+    else:
+        raise FileNotFoundError(f'文件{raw_fp}未找到，请正确配置导入的源文件路径！')
 
 
 def get_remove_unnamed_columns(raw_columns_list) -> list:
@@ -148,62 +190,188 @@ def strip_df_values(df: PdDataFrame) -> PdDataFrame:
     return df
 
 
-def analysis_operate(comment_info: str) -> tuple:
+def _deal_transfer(comment_str: str):
+    """
+    处理基金转换的操作
+    :param comment_str:
+    :return:
+    """
+    comment_list = comment_str.split('-')
+    split_head, from2target = comment_list
+    if TRANSFER_SYMBOL in from2target:
+        '''
+        一个转换行为实际由两部分组成：
+        1. 从源产品卖出到现金
+        2. 从现金买入目的产品
+        '''
+        from_name, target_name = from2target.split(TRANSFER_SYMBOL)
+        op_type = FundOpTypeEnum.transfer.label
+    elif from2target in [
+            ANT_FORTUNE_AUTO_TO_YEB, ANT_FORTUNE_BANK_CARD_TO_YEB, ANT_FORTUNE_SALE_TO_YEB, ANT_FORTUNE_QIAN_MGR_TO_YEB,
+            ANT_FORTUNE_BANK_CARD_BIG_TO_YEB, ANT_FORTUNE_BANK_CARD_SALARY_TO_YEB, ANT_FORTUNE_BANK_CARD_IN_STR
+    ]:
+        from_name = REAL_CASH
+        op_type = FundOpTypeEnum.purchase.label
+        if split_head != YLB_NAME:
+            # 1. 发的小红包/好友转账、2. 主动转入 3. 黄金票等提现（也是红包）4. 银行卡定期扣款
+            target_name = YEB_NAME
+        else:
+            target_name = YLB_NAME
+    elif from2target in [
+            ANT_FORTUNE_OUT_TO_BANK_CARD, ANT_FORTUNE_COMB_YEB_TO_YE, ANT_FORTUNE_MYXY_MANUAL_TO_YEB,
+            ANT_FORTUNE_MYXY_AUTO_TO_YEB
+    ]:
+        from_name = YEB_NAME
+        op_type = FundOpTypeEnum.sale.label
+        target_name = REAL_CASH
+    elif from2target == ANT_FORTUNE_TRANSFER_YEB_STR:
+        from_name = YEB_NAME_OLD
+        op_type = FundOpTypeEnum.transfer.label
+        target_name = YEB_NAME_NEW
+    elif split_head in [ANT_FORTUNE_YLB_TO_YEB_YE, ANT_FORTUNE_WS_INTEREST_TO_YEB]:
+        from_name = REAL_CASH
+        op_type = FundOpTypeEnum.purchase.label
+        target_name = YEB_NAME
+    elif split_head in [ANT_FORTUNE_SALE_PROD_TO_YEB, ANT_FORTUNE_REGULAR_INVEST_SALE_STR]:
+        from_name = f'理财产品<{from2target}>'
+        op_type = FundOpTypeEnum.sale.label
+        target_name = YEB_NAME
+    elif split_head in [ANT_FORTUNE_PURCHASE_YEB_TO_PROD, ANT_FORTUNE_REGULAR_INVEST_STR]:
+        from_name = YEB_NAME
+        op_type = FundOpTypeEnum.purchase.label
+        target_name = f'理财产品<{from2target}>'
+    elif split_head == ANT_FORTUNE_YEB_YE_TO_YLB:
+        if from2target == ANT_FORTUNE_PURCHASE_FUND_STR:
+            from_name = REAL_CASH
+            op_type = FundOpTypeEnum.sale.label
+            target_name = YLB_NAME
+        else:
+            raise NotSupportError(f'暂时无法处理交易行为：{comment_str}')
+    elif split_head == ANT_FORTUNE_CHALLENGE_RATE_TO_YEB:
+        # FIXME:收益挑战实际是从产品卖出到余额宝的过程
+        if ANT_FORTUNE_CHALLENGE_SALE_STR in from2target:
+            from_name = REAL_CASH
+            op_type = FundOpTypeEnum.sale.label
+            target_name = YEB_NAME
+        elif ANT_FORTUNE_CHALLENGE_PURCHASE_STR in from2target:
+            from_name = REAL_CASH
+            op_type = FundOpTypeEnum.purchase.label
+            target_name = YEB_NAME
+        else:
+            raise NotSupportError(f'暂时无法处理交易行为：{comment_str}')
+    else:
+        raise NotSupportError(f'暂时无法处理交易行为：{comment_str}')
+    return op_type, from_name, target_name
+
+
+def _deal_enum_operate(comment_str):
+    """
+    买入、分红、卖出操作
+    :param comment_str:
+    :return:
+    """
+    comment_list = comment_str.split('-')
+    head_comt, mid_comt, tail_comt = comment_list
+    # ['卖出至余额宝', '买入', '现金分红至余额宝', '转出至余额宝']
+    if tail_comt == ANT_FORTUNE_COMB_TO_YEB:
+        from_name = f'组合产品<{mid_comt}>'
+        op_type = FundOpTypeEnum.purchase.label
+        target_name = YEB_NAME
+        # raise NotSupportError(f'暂时无法处理<组合卖出>：{comment_str}')
+    elif tail_comt == ANT_FORTUNE_SALE_TO_YEB:
+        from_name = mid_comt
+        op_type = FundOpTypeEnum.sale.label
+        target_name = YEB_NAME
+    elif tail_comt == ANT_FORTUNE_PURCHASE_STR:
+        from_name = YEB_NAME
+        op_type = FundOpTypeEnum.purchase.label
+        target_name = mid_comt
+    elif tail_comt == ANT_FORTUNE_BONUS_TO_YEB:
+        from_name = mid_comt
+        op_type = FundOpTypeEnum.bonus.label
+        target_name = YEB_NAME
+    elif tail_comt in [ANT_FORTUNE_RE_BUY_TO_YEB, ANT_FORTUNE_BANK_CARD_SALARY_TO_YEB]:
+        from_name = REAL_CASH
+        op_type = FundOpTypeEnum.purchase.label
+        target_name = YEB_NAME
+    elif mid_comt == ANT_FORTUNE_BBZ_PART1_STR and tail_comt == ANT_FORTUNE_BBZ_PART2_STR:
+        from_name = REAL_CASH
+        op_type = FundOpTypeEnum.purchase.label
+        target_name = YEB_NAME
+    else:
+        raise NotSupportError(f'暂时无法处理交易行为：{comment_str}, tail_comt:{tail_comt}')
+    return op_type, from_name, target_name
+
+
+def _deal_complex_prod(comment_str: str):
+    """
+    有的商品名比较复杂，如：易方达黄金主题(QDII-LOF-FOF)A ，导致分割出错
+    :param comment_str: 
+    :return: 
+    """
+    comment_list = comment_str.split('-')
+    _, *mid_comt_split_list, tail_comt = comment_list
+    if tail_comt == ANT_FORTUNE_SALE_TO_YEB:
+        op_type = FundOpTypeEnum.sale.label
+        # 易方达黄金主题(QDII-LOF-FOF)A
+        from_name = '-'.join(mid_comt_split_list)
+        target_name = YEB_NAME
+        return op_type, from_name, target_name
+    elif tail_comt == ANT_FORTUNE_PURCHASE_STR:
+        # 易方达黄金主题(QDII-LOF-FOF)A
+        from_name = YEB_NAME
+        op_type = FundOpTypeEnum.purchase.label
+        target_name = '-'.join(mid_comt_split_list)
+        return op_type, from_name, target_name
+    else:
+        raise NotSupportError(f'暂时无法处理交易行为：{comment_str}')
+
+
+def _deal_single_comment(comment_str: str):
+    comment_list = comment_str.split('-')
+    prod = comment_list[0]
+    if prod == ANT_FORTUNE_HB_REWARD_PURCHASE_STR:
+        from_name = '<红包奖励>'
+        op_type = FundOpTypeEnum.purchase.label
+        target_name = YEB_NAME
+    elif prod == ANT_FORTUNE_RECEIVED_TO_YEB:
+        from_name = REAL_CASH
+        op_type = FundOpTypeEnum.purchase.label
+        target_name = YEB_NAME
+    elif prod == ANT_FORTUNE_YEB_YE_TO_YLB:
+        from_name = REAL_CASH
+        op_type = FundOpTypeEnum.sale.label
+        target_name = YLB_NAME
+    else:
+        raise NotSupportError(f'暂时无法处理交易行为：{comment_str}')
+    return op_type, from_name, target_name
+
+
+def analysis_operate(comment_str: str) -> tuple:
     """
     分割说明文字，解析交易行为
     FIXME: PY3.10 match-case
     """
     op_type, from_name, target_name = None, None, None
-    comment_list = comment_info.split('-')
+    comment_list = comment_str.split('-')
     comt_len = len(comment_list)
-    if comt_len == 2:
-        _, from2target = comment_list
-        if TRANSFER_SYMBOL in from2target:
-            '''
-            一个转换行为实际由两部分组成：
-            1. 从源产品卖出到现金
-            2. 从现金买入目的产品
-            '''
-            from_name, target_name = from2target.split(TRANSFER_SYMBOL)
-            op_type = FundOpTypeEnum.transfer
-        else:
-            raise NotSupportError(f'暂时无法处理交易行为：{comment_info}')
+    if comt_len == 1:
+        op_type, from_name, target_name = _deal_single_comment(comment_str)
+    elif comt_len == 2:
+        op_type, from_name, target_name = _deal_transfer(comment_str)
     elif comt_len == 3:
-        _, mid_comt, tail_comt = comment_list
-        # ['卖出至余额宝', '买入', '现金分红至余额宝', '转出至余额宝']
-        if tail_comt == ANT_FORTUNE_COMB_TO_YEB:
-            raise NotSupportError(f'暂时无法处理<组合卖出>：{comment_info}')
-        elif tail_comt == ANT_FORTUNE_SALE_TO_YEB:
-            from_name = mid_comt
-            op_type = FundOpTypeEnum.sale.label
-            target_name = CASH_NAME
-        elif tail_comt == ANT_FORTUNE_PURCHASE_STR:
-            from_name = CASH_NAME
-            op_type = FundOpTypeEnum.purchase.label
-            target_name = mid_comt
-        elif tail_comt == ANT_FORTUNE_BONUS_TO_YEB:
-            from_name = mid_comt
-            op_type = FundOpTypeEnum.bonus.label
-            target_name = CASH_NAME
-        else:
-            raise NotSupportError(f'暂时无法处理交易行为：{comment_info}, tail_comt:{tail_comt}')
+        op_type, from_name, target_name = _deal_enum_operate(comment_str)
     elif comt_len == 4:
         '''
         卖出退款，此时我们对重新买入产品不感兴趣
         '''
         _, from_name, raw_to_name, transfer_status = comment_list
         op_type = FundOpTypeEnum.transfer_refund.label
-        target_name = CASH_NAME
+        target_name = YEB_NAME
     elif comt_len == 5:
-        _, *mid_comt_split_list, tail_comt = comment_list
-        if tail_comt == ANT_FORTUNE_SALE_TO_YEB:
-            op_type = FundOpTypeEnum.sale.label
-            # 易方达黄金主题(QDII-LOF-FOF)A
-            from_name = '-'.join(mid_comt_split_list)
-            target_name = CASH_NAME
-        else:
-            raise NotSupportError(f'暂时无法处理交易行为：{comment_info}')
-
+        op_type, from_name, target_name = _deal_complex_prod(comment_str)
+    else:
+        raise NotSupportError(f'暂时无法处理交易行为：{comment_str}')
     return op_type, from_name, target_name
 
 
@@ -232,12 +400,37 @@ def get_code_from_comment():
     pass
 
 
+def get_durations(dt: PdDataFrame) -> Optional[Dict]:
+    """ 获取账单起止日期"""
+    # 起始时间：[2021-01-01 00:00:00]    终止时间：[2021-12-31 23:59:59]
+    datetime_text = dt.iloc[-16:-15]['op_type'].to_list()[0]
+    data_reg = r'([0-2][0-9]{3})\-(0[1-9]|1[0-2])\-([0-2][0-9]|3[0-1]) ([0-1][0-9]|2[0-3]):([0-5][0-9])\:([0-5][' \
+               r'0-9])( ([\-\+]([0-1][0-9])\:([0-5][0-9])))?'
+    reg_mat = re.findall(data_reg, datetime_text)
+
+    def mk_date(date_tuple: tuple):
+        """
+        2021/12/31-235959
+        :param date_tuple:
+        :return:
+        """
+        return f'{date_tuple[0]}{date_tuple[1]}{date_tuple[2]}-{date_tuple[3]}{date_tuple[4]}{date_tuple[5]}'
+
+    date_dict = None
+    if reg_mat:
+        date_list = list()
+        for date_item in reg_mat:
+            reformat_datetime = mk_date(date_item)
+            date_list.append(reformat_datetime)
+        date_dict = dict(zip(['start_datetime', 'end_datetime'], date_list))
+    return date_dict
+
+
 def pre_prepared_df(df: PdDataFrame) -> PdDataFrame:
     """
     对脏数据进行预处理：
     ## 行：
-    1. 去掉表头的 whitespace
-    2. 去掉尾部注释性文字（包含用户隐私数据）
+    去掉表头的 whitespace
     ## 列：
     1. 去掉最后一列无意义空列
     :param df:
@@ -249,9 +442,7 @@ def pre_prepared_df(df: PdDataFrame) -> PdDataFrame:
     removed_unnamed_columns_df = striped_columns_df[removed_unnamed_columns_list]
     rename_dict = mk_rename_dict(removed_unnamed_columns_list)
     renamed_df = removed_unnamed_columns_df.rename(columns=rename_dict)
-
-    useful_df = df_drop_useless_tail(renamed_df)
-    return useful_df
+    return renamed_df
 
 
 def df_drop_useless_tail(df: PdDataFrame, n: int = 20):
@@ -267,12 +458,22 @@ def df_drop_useless_tail(df: PdDataFrame, n: int = 20):
 
 def main():
     """
-
     :return:
     """
-    raw_df = get_raw_df()
+    if IS_FROM_PC:
+        source_fp = PC_ALIPAY_RECORDS_FP
+        drop_columns = []
+    else:
+        source_fp = MOBILE_ALIPAY_RECORDS_FP
+        drop_columns = MOBILE_DROP_COLUMNS
 
-    useful_df = pre_prepared_df(raw_df)
+    raw_df = get_raw_df(source_fp)
+
+    renamed_df = pre_prepared_df(raw_df)
+    date_dict = get_durations(renamed_df)
+    prefix = f'起始时间[{date_dict.get("start_datetime")}]-终止时间[{date_dict.get("end_datetime")}]'
+    # 去掉尾部注释性文字（包含用户隐私数据）
+    useful_df = df_drop_useless_tail(renamed_df)
     # 只保留基金交易数据
     striped_df = strip_df_values(useful_df)
     invest_df = filter_invest_df(striped_df)
@@ -281,11 +482,17 @@ def main():
     # 根据用户意愿删除流水号
     if not IS_RECORD_TRANSACTIONAL_NUMBER:
         analysis_df.drop(columns='trans_code', inplace=True)
+    # 删除无用字段
+    analysis_df.drop(columns=drop_columns, inplace=True)
 
     if IS_FROM_PC:
-        analysis_df.to_csv(PC_ALIPAY_EXPORT_FP, index=False)
+        with_date_name = f'{prefix}-{BASE_PC_FILE_EXPORT_NAME}'
+        alipay_export_fp = Path(current_path).joinpath(with_date_name)
     else:
-        analysis_df.to_csv(MOBILE_ALIPAY_EXPORT_FP, index=False)
+        with_date_name = f'{prefix}-{BASE_MOBILE_FILE_EXPORT_NAME}'
+        alipay_export_fp = Path(current_path).joinpath(with_date_name)
+
+    analysis_df.to_csv(alipay_export_fp, index=False)
     return 0
 
 
--- docs/guide/export-alipay.md
+++ docs/guide/export-alipay.md
@@ -1,20 +1,35 @@
 ---
 title: 导出支付宝理财记录
 ---
+## 手机端
+1. 支付宝 APP - 我的-账单右上角三个点-开具交易流水证明-用于个人对账。
+
+   或者
+
+2. 支付宝APP-理财-左上角小蚂蚁-对话框输入“交易记录导出”-根据视频提示操作即可。
+
+![](https://pic.imgdb.cn/item/61f3a63e2ab3f51d91e6bc45.png)
+
+:::tip
+如图所示，最好选择整月或者整年导出，以免漏记。
+:::
+## PC端
 1. 查询支付宝账号注销时间：[帮助文档](https://help.alipay.com/lab/help_detail.htm?help_id=247394)
 2. 点击[此链接](https://consumeprod.alipay.com/record/standard.htm) 登录支付宝；
    ```https://consumeprod.alipay.com/record/standard.htm```
 3. 查询账单信息
-   [![](https://pic.imgdb.cn/item/61e7ce262ab3f51d911a4b25.png)](https://pic.imgdb.cn/item/61e7ce262ab3f51d911a4b25.png)
+
+   ![](https://pic.imgdb.cn/item/61f3a1a22ab3f51d91e12a27.png)
 4. 下载压缩包
-   [![](https://pic.imgdb.cn/item/61e7cec82ab3f51d911ae260.png)](https://pic.imgdb.cn/item/61e7cec82ab3f51d911ae260.png)
+
+   ![](https://pic.imgdb.cn/item/61e7cec82ab3f51d911ae260.png)
 5. 解压文件
-6. 处理csv裸文件
+6. 处理csv源文件
    :::warning
    1. `IS_RECORD_TRANSACTIONAL_NUMBER`配置设置为`True`表明自愿将流水号作为记录的一部分上传到网站数据库，记录流水号涉及部分个人敏感数据上传，请确保使用时是自主配置该项。
    记录流水号有助于后期根据流水号去源网站回溯记录，操作示意如下：
    ![](https://pic.imgdb.cn/item/61f2603e2ab3f51d91af98a2.png)
-   2. 关于隐私泄露，根据流水号查询账单详情在支付宝网站属于敏感操作，网站会通过二次扫码进行用户鉴权，所以不用担心资产安全及隐私泄露问题。
+   2. 关于隐私问题，根据流水号查询账单详情在支付宝网站属于敏感操作，网站会通过二次扫码进行用户鉴权，所以不用过分担心资产安全及隐私泄露问题。
    :::
 7. 导入网站
 8. 查看导入记录
