e7775b32ad52209fc41f9f59336e235df7bbd042
imoyao <emailme8@163.com>
2022-03-01 18:27:01 +0800

    修复写入数据库时的段错误（FeeTypeEnum的in处理）

--- backend/fundmate/data/fundb/base.py
+++ backend/fundmate/data/fundb/base.py
@@ -10,7 +10,7 @@ from xalpha.cons import JSONDecodeError, rpost_json
 from backend.fundmate import settings
 from backend.fundmate.data.utils import base as dt_utils
 from backend.fundmate.data.utils import ratio
-from backend.fundmate.excepts import CrawlerException
+from backend.fundmate.excepts import CrawlerException, ParseError
 from backend.fundmate.exts.flask_loguru import logger
 
 _HEADER_STR = '''authority: api.jiucaishuo.com
@@ -206,7 +206,7 @@ class FundFeeRatio(ratio.BaseRatio):
             if redeem:
                 try:
                     redeem_info = self.redeem_rate(redeem)
-                except ValueError:
+                except (ValueError, ParseError):
                     raise CrawlerException(f'基金 {fund_code} 处理赎回信息 {redeem} 出错！')
 
             if to_db:
@@ -215,7 +215,7 @@ class FundFeeRatio(ratio.BaseRatio):
                 if redeem_info:
                     try:
                         self.save_fee_info(fund_code, redeem_info, fee_type=settings.FeeTypeEnum.redeem)
-                    except TypeError as e:
+                    except (TypeError, ParseError) as e:
                         raise CrawlerException(f'基金 {fund_code} 保存赎回信息 {redeem_info} 出错，出错信息：{e}！')
 
             info = {
