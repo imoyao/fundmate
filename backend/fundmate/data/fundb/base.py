e327efab085abc6a17ab7bd9e149176ec73dc054
imoyao <emailme8@163.com>
2022-07-13 23:08:14 +0800

    引入retry重试机制，对于返回错误进行重试 close #477

--- backend/fundmate/data/fundb/base.py
+++ backend/fundmate/data/fundb/base.py
@@ -5,7 +5,10 @@ import random
 import time
 from typing import Optional
 
-from xalpha.cons import JSONDecodeError, rpost_json
+from requests.exceptions import JSONDecodeError as RJSONDecodeError
+from retry import retry
+from xalpha.cons import JSONDecodeError as XJSONDecodeError
+from xalpha.cons import rpost_json
 
 from backend.fundmate import settings
 from backend.fundmate.data.utils import base as dt_utils
@@ -13,7 +16,8 @@ from backend.fundmate.data.utils import ratio
 from backend.fundmate.excepts import CrawlerException, IsClosedDurationError, ParseError
 from backend.fundmate.exts.flask_loguru import logger
 
-_HEADER_STR = '''authority: api.jiucaishuo.com
+# noqa: Q001
+_HEADER_STR = """authority: api.jiucaishuo.com
 method: POST
 path: /v2/kjtl/getbasedata
 scheme: https
@@ -29,30 +33,47 @@ sec-ch-ua-mobile: ?0
 sec-fetch-dest: empty
 sec-fetch-mode: cors
 sec-fetch-site: cross-site
-user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107''' \
-              '''Safari/537.36 Edg/92.0.902.55'''
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107""" \
+              """Safari/537.36 Edg/92.0.902.55"""
+
+
+def delay_timeout() -> float:
+    """
+    随机延时，参阅：
+    [python - How to get a random number between a float range? - Stack Overflow]
+    (https://stackoverflow.com/questions/6088077/how-to-get-a-random-number-between-a-float-range)
+
+    :return:
+    """
+    timeout = round(random.uniform(0.3, 0.7), 2)
+    return timeout
 
 
 class FundDB:
 
+    @retry(exceptions=(XJSONDecodeError, RJSONDecodeError, CrawlerException),
+           tries=10,
+           delay=delay_timeout(),
+           backoff=2,
+           max_delay=20)
     def kjtl(self, is_full: bool = True) -> dict:
         """
         恐惧贪婪指数
         """
         fear_href = 'https://funddb.cn/tool/fear'
         url = 'https://api.jiucaishuo.com/v2/kjtl/getbasedata'
-        data = '''{"type":"pc","data_source":"xichou","version":"1.6.0",
+        data = """{"type":"pc","data_source":"xichou","version":"1.6.0",
         "authtoken":"ddQYkiZQ087Z5Kr+ER5CmQMFCpjuC/qW","act_time":1627638543548,"tirgkjfs":"08","abiokytke":"0b",
         "u54rg5d":"b3","kf54ge7":"e","tiklsktr4":"8","lksytkjh":"c14c","sbnoywr":"53","bgd7h8tyu54":"54",
         "y654b5fs3tr":"5","bioduytlw":"a","bd4uy742":"f","h67456y":"1c1","bvytikwqjk":"54","ngd4uy551":"c1",
         "bgiuytkw":"9f","nd354uy4752":"d","ghtoiutkmlg":"513","bd24y6421f":"31","tbvdiuytk":"1","ibvytiqjek":"45",
         "jnhf8u5231":"9f","fjlkatj":"b35","hy5641d321t":"1f","iogojti":"1","ngd4yut78":"13","nkjhrew":"f",
-        "yt447e13f":"4","n3bf4uj7y7":"1","nbf4uj7y432":"0b","yi854tew":"2d","h13ey474":"2de","quikgdky":"7d"} '''
+        "yt447e13f":"4","n3bf4uj7y7":"1","nbf4uj7y432":"0b","yi854tew":"2d","h13ey474":"2de","quikgdky":"7d"}"""
         hd = dt_utils.parse_headers(_HEADER_STR)
         try:
             resp = rpost_json(url, headers=hd, data=data)
-        except JSONDecodeError:
-            raise CrawlerException('对方反爬机制导致错误，请稍候重试……')
+        except (XJSONDecodeError, RJSONDecodeError):
+            raise CrawlerException('对方反爬机制导致错误，请稍候重试……') from (XJSONDecodeError, RJSONDecodeError)
 
         info = None
         if resp and resp.get('code') == 0:
@@ -94,6 +115,11 @@ class FundDB:
 
 class FundFeeRatio(ratio.BaseRatio):
 
+    @retry(exceptions=(XJSONDecodeError, RJSONDecodeError, CrawlerException),
+           tries=5,
+           delay=delay_timeout(),
+           backoff=2,
+           max_delay=5)
     def rate(self, fund_code: str, to_db: bool = False) -> Optional[dict]:
         """
         origin: https://funddb.cn/site/fund_details?fund_code=001714
@@ -103,62 +129,54 @@ class FundFeeRatio(ratio.BaseRatio):
         _url = 'https://api.jiucaishuo.com/v2/fund-lists/fundrate'
         t = time.time()
         data = {
-            "code": fund_code,
-            "type": "pc",
-            "data_source": "xichou",
-            "version": "1.6.0",
-            "authtoken": "ddQYkiZQ087Z5Kr+ER5CmQMFCpjuC/qW",
-            "act_time": int(round(t * 1000)),
-            "tirgkjfs": "32",
-            "abiokytke": "86",
-            "u54rg5d": "5c",
-            "kf54ge7": "4",
-            "tiklsktr4": "2",
-            "lksytkjh": "8425",
-            "sbnoywr": "48",
-            "bgd7h8tyu54": "5f",
-            "y654b5fs3tr": "0",
-            "bioduytlw": "8",
-            "bd4uy742": "4",
-            "h67456y": "384",
-            "bvytikwqjk": "5f",
-            "ngd4uy551": "84",
-            "bgiuytkw": "69",
-            "nd354uy4752": "1",
-            "ghtoiutkmlg": "0c6",
-            "bd24y6421f": "8d",
-            "tbvdiuytk": "3",
-            "ibvytiqjek": "da",
-            "jnhf8u5231": "69",
-            "fjlkatj": "5cd",
-            "hy5641d321t": "d4",
-            "iogojti": "d",
-            "ngd4yut78": "c6",
-            "nkjhrew": "4",
-            "yt447e13f": "e",
-            "n3bf4uj7y7": "4",
-            "nbf4uj7y432": "86",
-            "yi854tew": "51",
-            "h13ey474": "514",
-            "quikgdky": "fe"
+            'code': fund_code,
+            'type': 'pc',
+            'data_source': 'xichou',
+            'version': '1.6.0',
+            'authtoken': 'ddQYkiZQ087Z5Kr+ER5CmQMFCpjuC/qW',
+            'act_time': int(round(t * 1000)),
+            'tirgkjfs': '32',
+            'abiokytke': '86',
+            'u54rg5d': '5c',
+            'kf54ge7': '4',
+            'tiklsktr4': '2',
+            'lksytkjh': '8425',
+            'sbnoywr': '48',
+            'bgd7h8tyu54': '5f',
+            'y654b5fs3tr': '0',
+            'bioduytlw': '8',
+            'bd4uy742': '4',
+            'h67456y': '384',
+            'bvytikwqjk': '5f',
+            'ngd4uy551': '84',
+            'bgiuytkw': '69',
+            'nd354uy4752': '1',
+            'ghtoiutkmlg': '0c6',
+            'bd24y6421f': '8d',
+            'tbvdiuytk': '3',
+            'ibvytiqjek': 'da',
+            'jnhf8u5231': '69',
+            'fjlkatj': '5cd',
+            'hy5641d321t': 'd4',
+            'iogojti': 'd',
+            'ngd4yut78': 'c6',
+            'nkjhrew': '4',
+            'yt447e13f': 'e',
+            'n3bf4uj7y7': '4',
+            'nbf4uj7y432': '86',
+            'yi854tew': '51',
+            'h13ey474': '514',
+            'quikgdky': 'fe'
         }
         hd = dt_utils.parse_headers(_HEADER_STR)
         # [httprequest - Python Request Post with param data - Stack Overflow]
         # (https://stackoverflow.com/questions/15900338/python-request-post-with-param-data)
         try:
             resp = rpost_json(_url, headers=hd, json=data)
-        except JSONDecodeError:
-            # 爬太快，数据处理不过来？
-            # [python - How to get a random number between a float range? - Stack Overflow]
-            # (https://stackoverflow.com/questions/6088077/how-to-get-a-random-number-between-a-float-range)
-            timeout = round(random.uniform(0.3, 0.7), 2)
-            time.sleep(timeout)
-            try:
-                resp = rpost_json(_url, headers=hd, json=data)
-            except JSONDecodeError:
-                msg = f'基金编码：{fund_code}反爬机制导致错误，请稍候重试……'
-                logger.error(msg)
-                raise CrawlerException(msg)
+        except (XJSONDecodeError, RJSONDecodeError):
+            msg = f'基金编码：{fund_code}反爬机制导致错误，请稍候重试……'
+            logger.error(msg)
+            raise CrawlerException(msg) from (XJSONDecodeError, RJSONDecodeError)
 
         code = resp.get('code')
         if code == 0:
@@ -166,7 +184,8 @@ class FundFeeRatio(ratio.BaseRatio):
             purchase = data.get('sg')
             op = data.get('gl')
             redeem = data.get('sh')
-            '''
+            """
+            示例数据
             {'buy': [{'money': '购买金额 < 100万',
                'time': '',
                'source': '1.50%',
@@ -188,8 +207,7 @@ class FundFeeRatio(ratio.BaseRatio):
               {'name': '销售服务费率', 'val': '0.00% (每年)'}],
              'redeem': [{'money': '', 'time': '持有期限 < 7天', 'source': '', 'rate': '1.50%'},
               {'money': '', 'time': '持有期限 ≥ 7天', 'source': '', 'rate': '0.50%'}]}
-
-            '''
+            """
             logger.info(f'Fund: {fund_code}, purchase: {purchase}, redeem: {redeem}')
 
             purchase_info = None
@@ -198,12 +216,13 @@ class FundFeeRatio(ratio.BaseRatio):
                 try:
                     purchase_info = self.purchase_rate(purchase, money_key='money', rate_key='rate')
                 except ValueError:
-                    raise CrawlerException(f'基金 {fund_code} 处理申购信息 {purchase} 出错！')
+                    raise CrawlerException(f'基金 {fund_code} 处理申购信息 {purchase} 出错！') from ValueError
             if redeem:
                 try:
                     redeem_info = self.redeem_rate(redeem)
                 except (ValueError, ParseError, IsClosedDurationError):
-                    raise CrawlerException(f'基金 {fund_code} 处理赎回信息 {redeem} 出错！')
+                    raise CrawlerException(f'基金 {fund_code} 处理赎回信息 {redeem} 出错！') from (ValueError, ParseError,
+                                                                                        IsClosedDurationError)
 
             if to_db:
                 if purchase_info:
@@ -212,7 +231,8 @@ class FundFeeRatio(ratio.BaseRatio):
                     try:
                         self.save_fee_info(fund_code, redeem_info, fee_type=settings.FeeTypeEnum.redeem)
                     except (TypeError, ParseError) as e:
-                        raise CrawlerException(f'基金 {fund_code} 保存赎回信息 {redeem_info} 出错，出错信息：{e}！')
+                        raise CrawlerException(f'基金 {fund_code} 保存赎回信息 {redeem_info} 出错，出错信息：{e}！') from (TypeError,
+                                                                                                          ParseError)
 
             info = {
                 'purchase': purchase_info,
