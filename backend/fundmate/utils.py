9bb0b6e9ecaf2fe6969bee67b8b1be5ea73630e5
imoyao <immoyao@gmail.com>
2022-09-29 18:07:14 +0800

    对于logging文件每次重启时进行重命名，保证进程唯一性 close #622

--- backend/fundmate/utils.py
+++ backend/fundmate/utils.py
@@ -9,13 +9,22 @@ import sys
 import time
 from collections import defaultdict
 from datetime import datetime, timedelta
-from pathlib import Path
+from pathlib import Path, PurePath
 from typing import Dict, List, Optional, Union
 
 import dateparser
 import pendulum
 
 from backend.fundmate.exts.flask_loguru import logger
+
+
+def logger_add_ext_before_suffix(logger_file_name: str):
+    """
+    日志文件添加时间戳
+    """
+    file_stem = PurePath(logger_file_name).stem
+    file_suffix = PurePath(logger_file_name).suffix
+    return f'{file_stem}-{int(time.time())}{file_suffix}'
 
 
 def convert_readable_days(number_of_days: int) -> tuple:
